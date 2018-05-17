acc6f2ad7a92fd11c6be862522d4acf977e46afe
jaynification <39388656+jaynification@users.noreply.github.com>
2018-05-17 19:20:41 -0300

    Added styles and scripts

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,7 @@
 <head>
 <meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<META NAME="viewport" content="width=device-width, initial-scale=1">
 <META NAME="author" CONTENT="The United Pagan Collective">
 <META NAME="subject" CONTENT="Community">
 <META NAME="Description" CONTENT="The United Pagan Collective is a legal not for profit entity dedicated solely to the support and promotion of Paganism in the province of New Brunswick.">
@@ -13,5 +15,16 @@
 <META NAME="distribution" CONTENT="Global">
 <META NAME="city" CONTENT="Moncton">
 <META NAME="country" CONTENT="Canada">
+
 <title>The United Pagan Collective</title>
+
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
 </head>
