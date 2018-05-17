fa01f0aff5c20a719976131289afdd77c98de8fc
jaynification <39388656+jaynification@users.noreply.github.com>
2018-05-17 19:08:58 -0300

    Initial Setup

---
+++ includes/head.php
@@ -0,0 +1,17 @@
+<head>
+<meta charset="UTF-8">
+<META NAME="author" CONTENT="The United Pagan Collective">
+<META NAME="subject" CONTENT="Community">
+<META NAME="Description" CONTENT="The United Pagan Collective is a legal not for profit entity dedicated solely to the support and promotion of Paganism in the province of New Brunswick.">
+<META NAME="Classification" CONTENT="The United Pagan Collective is a legal not for profit entity dedicated solely to the support and promotion of Paganism in the province of New Brunswick.">
+<META NAME="Keywords" CONTENT="paganism, religion, wiccan, druid, occult, witchcraft, heathenry, nature, gods, goddesses, community">
+<META NAME="Geography" CONTENT="531 Main St, Moncton, NB E1C 1C5">
+<META NAME="Language" CONTENT="English">
+<META NAME="Copyright" CONTENT="The United Pagan Collective">
+<META NAME="Designer" CONTENT="Sarah Tooley">
+<META NAME="Publisher" CONTENT="Sarah Tooley">
+<META NAME="distribution" CONTENT="Global">
+<META NAME="city" CONTENT="Moncton">
+<META NAME="country" CONTENT="Canada">
+<title>The United Pagan Collective</title>
+</head>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<?php
+  include './includes/head.php';
+  include './includes/header.php';
+ ?>
+
+<body>
+Content of the document......
+</body>
+
+<?php 
+  include './includes/footer.php';
+ ?>
+</html>
